a59e9c5fdc2e23a03ead57d1d9619f6e495b0387
h3lpkey <h3lpkey@gmail.com>
2022-04-13 12:52:08 +0500

    test(lockCollection): uncommend file

--- tests/src/lockCollection.test.ts
+++ tests/src/lockCollection.test.ts
@@ -1,49 +1,49 @@
-// import { getApiConnection } from "./substrate/substrate-api";
-// import { expectTxFailure } from "./util/helpers";
-// import { createCollection, lockCollection } from "./util/tx";
+import { getApiConnection } from "./substrate/substrate-api";
+import { expectTxFailure } from "./util/helpers";
+import { createCollection, lockCollection } from "./util/tx";
 
-// describe("Integration test: lock collection", () => {
-//   const Alice = "//Alice";
-//   const Bob = "//Bob";
+describe("Integration test: lock collection", () => {
+  const Alice = "//Alice";
+  const Bob = "//Bob";
 
-//   let api: any;
-//   before(async () => {
-//     api = await getApiConnection();
-//   });
+  let api: any;
+  before(async () => {
+    api = await getApiConnection();
+  });
 
-//   it("Lock collection", async () => {
-//     await createCollection(
-//       api,
-//       Alice,
-//       "test-metadata",
-//       null,
-//       "test-symbol"
-//     ).then(async (collectionId) => {
-//       await lockCollection(api, Alice, collectionId);
-//     });
-//   });
+  it("Lock collection", async () => {
+    await createCollection(
+      api,
+      Alice,
+      "test-metadata",
+      null,
+      "test-symbol"
+    ).then(async (collectionId) => {
+      await lockCollection(api, Alice, collectionId);
+    });
+  });
 
-//   it("[Negative] Lock non-existing NFT collection", async () => {
-//     const tx = lockCollection(api, Alice, 99999);
-//     await expectTxFailure(/rmrkCore.CollectionUnknown/, tx);
-//   });
+  it("[Negative] Lock non-existing NFT collection", async () => {
+    const tx = lockCollection(api, Alice, 99999);
+    await expectTxFailure(/rmrkCore.CollectionUnknown/, tx);
+  });
 
-//   it("[Negative] Lock not an owner NFT collection issuer", async () => {
-//     await createCollection(
-//       api,
-//       Alice,
-//       "test-metadata",
-//       null,
-//       "test-symbol"
-//     ).then(async (collectionId) => {
-//       const tx = lockCollection(api, Bob, collectionId);
-//       await expectTxFailure(/rmrkCore.CollectionUnknown/, tx);
+  it("[Negative] Lock not an owner NFT collection issuer", async () => {
+    await createCollection(
+      api,
+      Alice,
+      "test-metadata",
+      null,
+      "test-symbol"
+    ).then(async (collectionId) => {
+      const tx = lockCollection(api, Bob, collectionId);
+      await expectTxFailure(/rmrkCore.CollectionUnknown/, tx);
 
-//       // await lockCollection(api, Alice, collectionId);
-//     });
-//   });
+      // await lockCollection(api, Alice, collectionId);
+    });
+  });
 
-//   after(() => {
-//     api.disconnect();
-//   });
-// });
+  after(() => {
+    api.disconnect();
+  });
+});
