7ea378f51cb5758dab813d61e2a2026e5e312eab
h3lpkey <h3lpkey@gmail.com>
2022-05-12 10:37:42 +0400

    test(addNftResource): add tests

--- tests/src/addNftResource.test.ts
+++ tests/src/addNftResource.test.ts
@@ -1,13 +1,24 @@
 import { getApiConnection } from "./substrate/substrate-api";
+import { NftIdTuple } from "./util/fetch";
+import { expectTxFailure } from "./util/helpers";
 import {
+  acceptNft,
   addNftResource,
+  createBase,
   createCollection,
-  mintNft
+  mintNft,
+  sendNft,
 } from "./util/tx";
 
 describe("Integration test: Add top-level NFT resource (by the same user)", () => {
   const Alice = "//Alice";
-  const resouceId = "resid0";
+  const Bob = "//Bob";
+  const resourceId = "resid0";
+  const slotId = "1";
+  const slotSrc = "test-slot";
+  const nonexistentId = 99999;
+  let nftAliceId: number;
+  let baseId: number;
 
   let api: any;
   before(async () => {
@@ -15,14 +26,14 @@ describe("Integration test: Add top-level NFT resource (by the same user)", () =
   });
 
   it("Add resource", async () => {
-    await createCollection(
+    const collectionIdAlice = await createCollection(
       api,
       Alice,
       "test-metadata",
       null,
       "test-symbol"
     ).then(async (collectionId) => {
-      const nftId = await mintNft(
+      nftAliceId = await mintNft(
         api,
         Alice,
         Alice,
@@ -30,8 +41,217 @@ describe("Integration test: Add top-level NFT resource (by the same user)", () =
         "nft-metadata"
       );
 
-      await addNftResource(api, nftId, resouceId, collectionId, null, Alice, null);
+      return collectionId;
     });
+
+    await createCollection(api, Bob, "test-metadata", null, "test-symbol").then(
+      async (collectionIdBob) => {
+        const nftBob = await mintNft(
+          api,
+          Bob,
+          Bob,
+          collectionIdBob,
+          "nft-metadata"
+        );
+        const newOwnerNFT: NftIdTuple = [collectionIdAlice, nftAliceId];
+
+        await sendNft(
+          api,
+          "pending",
+          Bob,
+          collectionIdBob,
+          nftBob,
+          newOwnerNFT
+        );
+        await acceptNft(api, Alice, collectionIdBob, nftBob, newOwnerNFT);
+
+        baseId = await createBase(api, Alice, "test-base", "DTBase", [
+          {
+            SlotPart: {
+              id: slotId,
+              equippable: "All",
+              z: 1,
+              src: slotSrc,
+            },
+          },
+        ]);
+
+        await addNftResource(
+          api,
+          nftBob,
+          resourceId,
+          collectionIdAlice,
+          baseId.toString(),
+          Alice,
+          slotId
+        );
+      }
+    );
+  });
+
+  it("Negative: unable to accept a non-existing resource", async () => {
+    const collectionIdAlice = await createCollection(
+      api,
+      Alice,
+      "test-metadata",
+      null,
+      "test-symbol"
+    ).then(async (collectionId) => {
+      nftAliceId = await mintNft(
+        api,
+        Alice,
+        Alice,
+        collectionId,
+        "nft-metadata"
+      );
+
+      return collectionId;
+    });
+
+    await createCollection(api, Bob, "test-metadata", null, "test-symbol").then(
+      async (collectionIdBob) => {
+        const nftBob = await mintNft(
+          api,
+          Bob,
+          Bob,
+          collectionIdBob,
+          "nft-metadata"
+        );
+        const newOwnerNFT: NftIdTuple = [collectionIdAlice, nftAliceId];
+
+        await sendNft(
+          api,
+          "pending",
+          Bob,
+          collectionIdBob,
+          nftBob,
+          newOwnerNFT
+        );
+        await acceptNft(api, Alice, collectionIdBob, nftBob, newOwnerNFT);
+
+        baseId = await createBase(api, Alice, "test-base", "DTBase", [
+          {
+            SlotPart: {
+              id: slotId,
+              equippable: "All",
+              z: 1,
+              src: slotSrc,
+            },
+          },
+        ]);
+
+        const tx = addNftResource(
+          api,
+          nonexistentId,
+          resourceId,
+          collectionIdAlice,
+          baseId.toString(),
+          Alice,
+          slotId
+        );
+
+        await expectTxFailure(/rmrkCore.NoAvailableNftId/, tx);
+      }
+    );
+  });
+
+  it("Negative: unable to accept a resource by a not-an-NFT-owner user", async () => {
+    const collectionIdAlice = await createCollection(
+      api,
+      Alice,
+      "test-metadata",
+      null,
+      "test-symbol"
+    ).then(async (collectionId) => {
+      nftAliceId = await mintNft(
+        api,
+        Alice,
+        Alice,
+        collectionId,
+        "nft-metadata"
+      );
+
+      return collectionId;
+    });
+
+    await createCollection(api, Bob, "test-metadata", null, "test-symbol").then(
+      async (collectionIdBob) => {
+        const nftBob = await mintNft(
+          api,
+          Bob,
+          Bob,
+          collectionIdBob,
+          "nft-metadata"
+        );
+        const newOwnerNFT: NftIdTuple = [collectionIdAlice, nftAliceId];
+
+        await sendNft(
+          api,
+          "pending",
+          Bob,
+          collectionIdBob,
+          nftBob,
+          newOwnerNFT
+        );
+        const tx = acceptNft(api, Bob, collectionIdBob, nftBob, newOwnerNFT);
+
+        await expectTxFailure(/rmrkCore.NoPermission/, tx);
+      }
+    );
+  });
+
+  it("Negative: unable to accept a resource to not-a-target NFT", async () => {
+    const collectionIdAlice = await createCollection(
+      api,
+      Alice,
+      "test-metadata",
+      null,
+      "test-symbol"
+    ).then(async (collectionId) => {
+      nftAliceId = await mintNft(
+        api,
+        Alice,
+        Alice,
+        collectionId,
+        "nft-metadata"
+      );
+
+      return collectionId;
+    });
+
+    await createCollection(api, Bob, "test-metadata", null, "test-symbol").then(
+      async (collectionIdBob) => {
+        const nftBob = await mintNft(
+          api,
+          Bob,
+          Bob,
+          collectionIdBob,
+          "nft-metadata"
+        );
+        const newOwnerNFT: NftIdTuple = [collectionIdAlice, nftAliceId];
+
+        await sendNft(
+          api,
+          "pending",
+          Bob,
+          collectionIdBob,
+          nftBob,
+          newOwnerNFT
+        );
+
+        const newOwnerNFTerror: NftIdTuple = [collectionIdAlice, nonexistentId];
+
+        const tx = acceptNft(
+          api,
+          Alice,
+          collectionIdBob,
+          nftBob,
+          newOwnerNFTerror
+        );
+
+        await expectTxFailure(/rmrkCore.NoAvailableNftId/, tx);
+      }
+    );
   });
 
   after(() => {
