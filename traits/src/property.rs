4270632228e7d33465077ba97606701958fc2949
Daniel Shiposha <dshiposha@usetech.com>
2022-05-21 19:44:00 +0000

    feat: add property type

--- traits/src/property.rs
+++ traits/src/property.rs
@@ -1,6 +1,36 @@
 use sp_runtime::DispatchResult;
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
 
-use crate::primitives::*;
+#[cfg(feature = "std")]
+use serde::Serialize;
+
+use crate::{
+	primitives::*,
+	serialize,
+};
+
+#[cfg_attr(feature = "std", derive(Serialize))]
+#[derive(Encode, Decode, PartialEq, TypeInfo)]
+#[cfg_attr(
+	feature = "std",
+	serde(
+		bound = r#"
+			BoundedKey: AsRef<[u8]>,
+			BoundedValue: AsRef<[u8]>
+		"#
+	)
+)]
+pub struct PropertyInfo<BoundedKey, BoundedValue>
+{
+	/// Key of the property
+	#[cfg_attr(feature = "std", serde(with = "serialize::vec"))]
+	pub key: BoundedKey,
+
+	/// Value of the property
+	#[cfg_attr(feature = "std", serde(with = "serialize::vec"))]
+	pub value: BoundedValue,
+}
 
 /// Abstraction over a Property system.
 #[allow(clippy::upper_case_acronyms)]
