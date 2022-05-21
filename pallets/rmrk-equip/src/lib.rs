1aaf214d3c63b7556b7f429d48cfe62feb150ff6
Daniel Shiposha <dshiposha@usetech.com>
2022-05-21 19:34:06 +0000

    feat: make theme serializable

--- pallets/rmrk-equip/src/lib.rs
+++ pallets/rmrk-equip/src/lib.rs
@@ -16,8 +16,10 @@ pub use pallet::*;
 
 use rmrk_traits::{
 	primitives::*, AccountIdOrCollectionNftTuple, Base, BaseInfo, BasicResource,
-	ComposableResource, EquippableList, PartType, ResourceTypes, SlotResource, Theme,
+	ComposableResource, EquippableList, PartType, ResourceTypes, SlotResource, Theme, ThemeProperty,
 };
+
+use sp_std::vec::Vec;
 
 mod functions;
 
@@ -36,6 +38,11 @@ pub use pallet::*;
 pub type StringLimitOf<T> = BoundedVec<u8, <T as pallet_uniques::Config>::StringLimit>;
 
 pub type BoundedResource<T> = BoundedVec<u8, <T as pallet_rmrk_core::Config>::ResourceSymbolLimit>;
+
+pub type ThemeOf<T> = Theme<
+	StringLimitOf<T>,
+	Vec<ThemeProperty<StringLimitOf<T>>>
+>;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -337,7 +344,7 @@ pub mod pallet {
 		pub fn theme_add(
 			origin: OriginFor<T>,
 			base_id: BaseId,
-			theme: Theme<BoundedVec<u8, T::StringLimit>>,
+			theme: ThemeOf<T>,
 		) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
 
